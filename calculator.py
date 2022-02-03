db7e0319d6482dfd2ee2191f7895b355568e4b1c
Yesica N Mendoza <yesicanmendoza@gmail.com>
2022-02-03 05:59:11 +0000

    Adding all operations

--- calculator.py
+++ calculator.py
@@ -13,5 +13,26 @@ while True:
     elif token_input[0] == "pow":
         result = power(int(token_input[1]),int(token_input[2]))
         print(float(result))
-        
+    elif token_input[0] == "add":
+        result = add(int(token_input[1]),int(token_input[2]))
+        print(float(result))
+    elif token_input[0] == "subtract":
+        result = subtract(int(token_input[1]),int(token_input[2]))
+        print(float(result))
+    elif token_input[0] == "multiply":
+        result = multiply(int(token_input[1]),int(token_input[2]))
+        print(float(result))
+    elif token_input[0] == "divide":
+        result = divide(int(token_input[1]),int(token_input[2]))
+        print(float(result))
+    elif token_input[0] == "square":
+        result = square(int(token_input[1]))
+        print(float(result))
+    elif token_input[0] == "cube":
+        result = cube(int(token_input[1]))
+        print(float(result))
+    elif token_input[0] == "mod":
+        result = mod(int(token_input[1]),int(token_input[2]))
+        print(float(result))
+
 
